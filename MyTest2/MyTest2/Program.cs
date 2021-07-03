afbf508d5c17b7be26ffd028d1de8bc7e4f5f2e4
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-03 00:45:50 -0400

    First c# Program

---
+++ MyTest2/MyTest2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someText = "Here is some test";
+            int number = 10;
+            double anotherNumber = 10.9;
+            bool isOrdered = true;
+            string input = Console.ReadLine();
+            var anything = 10.9;
+
+            Console.WriteLine(someText);
+            Console.WriteLine(number);
+            Console.WriteLine(anotherNumber);
+            Console.WriteLine(isOrdered);
+            Console.WriteLine(input);
+            Console.WriteLine(anything);
+        }
+    }
+}
---
+++ MyTest2/UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6537921d-d81a-4a68-948f-2f2819206a61")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
