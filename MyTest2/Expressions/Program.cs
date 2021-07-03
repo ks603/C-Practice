dc5d75a357eac0105e04c063692578f3d579d011
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-03 11:24:19 -0400

    Practice with Expressions

---
+++ MyTest2/Expressions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int convertedInputToNumber;
+            int.TryParse(input, out convertedInputToNumber);
+
+            int number = 90;
+            int result = 10 + 10 - 100 + 1000 - number + convertedInputToNumber;
+            Console.WriteLine(result);
+        }
+    }
+}
