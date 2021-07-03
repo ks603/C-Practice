7c8dc8b998fdfa368d401bb249d6d1b8f807f025
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-03 11:37:06 -0400

    Order Of Operations

---
+++ MyTest2/Expressions2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = (20 + 5)* 2;
+            Console.WriteLine(result);
+
+            int test1 = 20 / 5 * 2;
+            Console.WriteLine(test1);
+        }
+    }
+}
