265e1c35751596d2737ff1710e302e4a17833fff
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-06 20:47:42 -0400

    String StringManipulation

--- StringManipulation/StringManipulation/Program.cs
+++ StringManipulation/StringManipulation/Program.cs
@@ -10,10 +10,31 @@ namespace StringManipulation
     {
         static void Main(string[] args)
         {
-            string someText = "Here is some text";
-            string someOtherText = "Here is some other text";
+            string someText = "Here is some Text";
+            string someOtherText = "Here is some text";
 
-            bool isEqual = someText.Equals(someOtherText, StringComparison.Ordinal);
+            bool isEqual = someText.Equals(someOtherText, StringComparison.OrdinalIgnoreCase);
+
+            string addedText = someText + " " + someOtherText + " here is some more text.";
+            string formattedString = string.Format("{0} {1} here is some more text.", someText, someOtherText);
+
+
+            Console.WriteLine(formattedString);
+
+            Console.WriteLine(someText.Length);
+            Console.WriteLine(someText[3]);
+
+            Console.WriteLine(someText.Substring(8, 4));
+
+            Console.WriteLine(someText.ToUpper());
+
+            Console.WriteLine(someOtherText.ToLower());
+
+            string anotherText = string.Empty;
+
+            string replacedText = someText.Replace("Text", "food");
+
+            Console.WriteLine(replacedText);
         }
     }
 }
