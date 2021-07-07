4cb501caa8ccc4229a0d00a0e27642381e6e950e
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-06 20:23:53 -0400

    String StringManipulation

---
+++ StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someText = "Here is some text";
+            string someOtherText = "Here is some other text";
+
+            bool isEqual = someText.Equals(someOtherText, StringComparison.Ordinal);
+        }
+    }
+}
