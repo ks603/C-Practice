4e8ce56f2512e3370dabc66c67507a9f29b77d34
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-06 23:04:27 -0400

    Static vs Non Static

--- StringManipulation/ObjectsClasses/Person.cs
+++ StringManipulation/ObjectsClasses/Person.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace ObjectsClasses
 {
-    class Person
+    public class Person
     {
+        private int _age = 25;
+        public int getAge()
+        {
+            return _age;
+        }
+
+        public void setAge(int newAge)
+        {
+            _age = newAge;
+        }
+
+        static public void greet()
+        {
+            Console.WriteLine("Hello!");
+        }
     }
 }
--- StringManipulation/ObjectsClasses/Program.cs
+++ StringManipulation/ObjectsClasses/Program.cs
@@ -10,6 +10,12 @@ namespace ObjectsClasses
     {
         static void Main(string[] args)
         {
+            Person person1 = new Person();
+            Console.WriteLine(person1.getAge());
+            person1.setAge(29);
+            Console.WriteLine(person1.getAge());
+
+            Person.greet();
         }
     }
 }
