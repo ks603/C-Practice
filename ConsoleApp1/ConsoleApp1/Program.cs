b9dbb8ded29f5e6adf66bb4ef602e4220811ba87
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-07 22:35:22 -0400

    First Exception

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int convertedNumber;
+            bool isConvertedSuccesfully = int.TryParse(input, out convertedNumber);
+
+            if (!isConvertedSuccesfully)
+            {
+                throw new Exception("Conversion was not succesful.");
+            }
+        }
+    }
+}
