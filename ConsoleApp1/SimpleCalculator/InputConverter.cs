7402abf921db2c8194af4766a1ae259f87aa8158
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-08 23:51:07 -0400

    Created SimpleCalculator

---
+++ ConsoleApp1/SimpleCalculator/InputConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+       public double ConvertInputToNumberic(string argTextInput)
+        {
+            double convertNumber;
+            if (!double.TryParse(argTextInput, out convertNumber)) throw new ArgumentException("Expected a numeric value.");
+            return convertNumber;
+        }
+    }
+}
