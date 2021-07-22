5594523d5e41c85017f7f7e38b90dccd0790bd85
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-21 22:17:42 -0400

    Do While Practice

--- Lists/Lists/Program.cs
+++ Lists/Lists/Program.cs
@@ -10,14 +10,23 @@ namespace Lists
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine();
-            Console.WriteLine("User input is {0}", input);
+            //var input = Console.ReadLine();
+            //Console.WriteLine("User input is {0}", input);
 
-            while (!input.Equals(string.Empty))
+            //while (!input.Equals(string.Empty))
+            //{
+            //    input = Console.ReadLine();
+            //    Console.WriteLine("User input is {0}", input);
+            //}
+
+            var input = string.Empty;
+
+            do
             {
                 input = Console.ReadLine();
                 Console.WriteLine("User input is {0}", input);
-            }
+
+            } while (!input.Equals(string.Empty));
         }
     }
 }
