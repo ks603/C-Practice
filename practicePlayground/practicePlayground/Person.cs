62cf6c9f87593593ee600bc1cde4bc3def4d8654
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-27 22:00:08 -0400

    Created Class Person

---
+++ practicePlayground/practicePlayground/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicePlayground
+{
+    class Person
+    {
+        public Person(string argFirstname, string argLastName)
+        {
+            FirstName = argFirstname;
+            LastName = argLastName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+    }
+}
---
+++ practicePlayground/practicePlayground/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicePlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("John", "Smith");
+
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
