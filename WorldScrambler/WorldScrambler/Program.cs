490e1295e403d734d3a9d1f5c3ddcf5999b24243
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-29 21:55:11 -0400

    Setup to ask user if theyw ant to continue

--- WorldScrambler/WorldScrambler/Program.cs
+++ WorldScrambler/WorldScrambler/Program.cs
@@ -10,6 +10,32 @@ namespace WorldScrambler
     {
         static void Main(string[] args)
         {
+            bool continueWordUnscramble = true;
+            do
+            {
+                Console.WriteLine("Please enter the option - F for File and M for Manual");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.Write("Enter scrembled words file name: ");
+                        ExecuteScrambledWordsInFileScenario();
+                        break;
+                    case "M":
+                        Console.Write("Enter scrambled words manually: ");
+                        ExecuteScrambledWordsInManualScenario();
+                        break;
+                    default:
+                        Console.Write("Option was not recongnized.");
+                        break;
+                }
+            } while ();
+        }
+
+        private static void ExecuteScrambledWordsInFileScenario()
+        {
+            throw new NotImplementedException();
         }
     }
 }
