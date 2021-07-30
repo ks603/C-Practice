afb1010c10e3dd26765e617d305b863f7a3aafa0
Kevin <kevin.shaughnessy18@gmail.com>
2021-07-29 21:26:37 -0400

    Start of Word Scrambler Project

---
+++ Test/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = { "First Line", "Second Line", "Third Line" };
+            string[] formattedLines = new string[lines.Length];
+
+            Console.WriteLine(formattedLines);
+
+        }
+    }
+}
