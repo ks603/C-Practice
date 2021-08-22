d83001d05edf1ef116e99de94e988cd6e78473a4
Kevin <kevin.shaughnessy18@gmail.com>
2021-08-22 15:33:40 -0400

    Print Googles html to the console

---
+++ UsingKeyword/UsingKeyword/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string googleMainPage = client.DownloadString("http://www.Google.com");
+                Console.WriteLine(googleMainPage);
+            }
+        }
+    }
+}
