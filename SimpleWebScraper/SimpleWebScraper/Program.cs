34a302bc23281e600042526223eb4806196ecc11
Kevin <kevin.shaughnessy18@gmail.com>
2021-08-27 22:42:14 -0400

    Created inputs and built URL request to craigslist

--- SimpleWebScraper/SimpleWebScraper/Program.cs
+++ SimpleWebScraper/SimpleWebScraper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,19 @@ namespace SimpleWebScraper
 {
     class Program
     {
+        private const string Method = "search";
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter which city you would like to scrape information from:");
+            var craigsListCity = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine("Please enter the Craiglist category that you would like to scrape");
+            var craigslistCategoryName = Console.ReadLine() ?? string.Empty;
+
+            using (WebClient client = new WebClient())
+            {
+                string content = client.DownloadString($"http://{craigsListCity.Replace(" ", string.Empty)}craigsListCity.org/{Method}/{craigslistCategoryName}");
+            }
         }
     }
 }
