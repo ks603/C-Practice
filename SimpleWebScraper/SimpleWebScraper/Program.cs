7a2bbd6393a7695a04c6b37293317c5656431a5c
Kevin <kevin.shaughnessy18@gmail.com>
2021-08-27 23:13:31 -0400

    Completed the web SimpleWebScraper

--- SimpleWebScraper/SimpleWebScraper/Program.cs
+++ SimpleWebScraper/SimpleWebScraper/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using SimpleWebScraper.Builders;
+using SimpleWebScraper.Data;
+using SimpleWebScraper.Workers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SimpleWebScraper
@@ -21,6 +25,33 @@ namespace SimpleWebScraper
             using (WebClient client = new WebClient())
             {
                 string content = client.DownloadString($"http://{craigsListCity.Replace(" ", string.Empty)}craigsListCity.org/{Method}/{craigslistCategoryName}");
+
+                ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                    .WithData(content)
+                    .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?</a>)")
+                    .WithRegexOption(RegexOptions.ExplicitCapture)
+                    .WithPart(new ScrapeCriteriaPartBuilder()
+                        .WithRegex(@">(.*?)</a>")
+                        .WithRegexOptions(RegexOptions.Singleline)
+                        .Build())
+                    .WithPart(new ScrapeCriteriaPartBuilder()
+                        .WithRegex(@"gref=\""(.*?)\""")
+                        .WithRegexOptions(RegexOptions.Singleline)
+                        .Build())
+                    .Build();
+
+                Scraper scraper = new Scraper();
+
+                var scrapedElements = scraper.Scrape(scrapeCriteria);
+
+                if (scrapedElements.Any())
+                {
+                    foreach (var scrapedElement in scrapedElements) Console.WriteLine(scrapedElement);
+                }
+                else
+                {
+                    Console.WriteLine("There were no matches for the specified scrape criteria.");
+                }
             }
         }
     }
