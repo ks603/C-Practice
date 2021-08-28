3c48f6bac0734dc7c382d569e08a3a9dd4608e61
Kevin <kevin.shaughnessy18@gmail.com>
2021-08-28 00:11:37 -0400

    Created unit test for SimpleWebScraper

---
+++ SimpleWebScraper/SimpleWebScraper.Test.Unit/Workers/ScraperTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleWebScraper.Workers;
+using SimpleWebScraper.Data;
+using SimpleWebScraper.Builders;
+using System.Text.RegularExpressions;
+
+namespace SimpleWebScraper.Test.Unit
+{
+	[TestClass]
+	public class ScraperTest
+	{
+		private readonly Scraper _scraper = new Scraper();
+
+		[TestMethod]
+		public void FindCollectionWithNoParts()
+		{
+			var content = "Some fluff data <a href=\"http://domain.com\" data-id=\"someId\" class=\"result-title hdrlnk\">some text</a> more fluff data";
+
+
+			ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+				.WithData(content)
+				.WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"" id=\""(.*?)\"" >(.*?)</a>")
+				.WithRegexOption(RegexOptions.ExplicitCapture)
+				.Build();
+
+			var foundElements = _scraper.Scrape(scrapeCriteria);
+
+			Assert.IsTrue(foundElements.Count == 1);
+			Assert.IsTrue(foundElements[0] == "<a href=\"http://domain.com\" data-id=\"someId\" class=\"result-title hdrlnk\">some text</a>");
+		}
+	}
+}
